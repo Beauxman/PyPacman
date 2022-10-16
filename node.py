a3a1cfe461f978819761f2b54f2959e4949aa5c0
Beauxman <76933735+Beauxman@users.noreply.github.com>
2022-10-15 20:07:15 -0700

    Implemented different node types

--- node.py
+++ node.py
@@ -10,13 +10,18 @@ class Node:
         self.center = center
         self.adjacent = adjacent
         self.type = type
-        self.size = 5
+
+        self.size = 1
+        if type == 1:
+            self.size = 5
+        elif type == 3:
+            self.size = 10
 
     def update(self):
         self.draw()
 
     def draw(self):
-        if self.type == 1:
+        if self.type != 0:# and self.type != 2: #NOTE COMMENTED OUT FOR SHOWING OTHERWISE INVISIBLE NODES
             pg.draw.circle(self.screen, (255, 255, 255), (self.center.x, self.center.y), self.size)
 
 class Nodes:
@@ -38,20 +43,19 @@ class Nodes:
         for i in range(0, len(self.locations)):
             curX = self.xOffset
             self.nodeList.append(list())
-            for j in range(0, len(self.locations[i])):
-                type = 0
+            for j in range(0, len(self.locations[i]) - 1):
+                type = int(self.locations[i][j])
                 point = Point(x=curX, y=curY)
                 adjacent = list()
-                if self.locations[i][j] == "1":
-                    if self.locations[i - 1][j] == "1":
+                if self.locations[i][j] != "0":
+                    if self.locations[i - 1][j] != "0":
                         adjacent.append("UP")
-                    if self.locations[i + 1][j] == "1":
+                    if self.locations[i + 1][j] != "0":
                         adjacent.append("DOWN")
-                    if self.locations[i][j - 1] == "1":
+                    if self.locations[i][j - 1] != "0":
                         adjacent.append("LEFT")
-                    if self.locations[i][j + 1] == "1":
+                    if self.locations[i][j + 1] != "0":
                         adjacent.append("RIGHT")
-                    type = 1
                 node = Node(game=self.game, center=point, adjacent=adjacent, type=type)
                 self.nodeList[i].append(node)
                 curX += self.xScale
